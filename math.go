cbdd81a40dae667e47da0709877d8324badeddcb
Rafael Holland <rafaelholland9@gmail.com>
2023-02-20 16:20:51 -0300

    fix: :sparkles: fix importing go file

--- math.go
+++ math.go
@@ -1,10 +1,11 @@
 package main
+
 import "fmt"
 
-func main(){
-	fmt.Println(Soma(10, 10))
+func main() {
+	fmt.Println(soma(112, 10))
 }
 
-func Soma(a int, b int){
-	return a+b
+func soma(a int, b int) int {
+	return a + b
 }
